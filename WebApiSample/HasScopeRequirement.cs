7548906470c1475b01f32e16699d6953be292a69
Alef <alef.carlos@gmail.com>
2022-04-22 00:41:10 -0300

    first commit

---
+++ WebApiSample/HasScopeRequirement.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+
+namespace WebApiSample
+{
+    public class HasScopeRequirement : IAuthorizationRequirement
+    {
+        public string Scope { get; }
+
+        public HasScopeRequirement(string scope)
+        {
+            Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+        }
+    }
+}
